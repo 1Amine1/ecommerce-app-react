4b0fe2c5786ee0e71fdf51d37c71513e1a4dd610
devarsh19 <devarsh96patel@gmail.com>
2020-12-22 23:39:47 -0700

    finished chargeUser flow by creating createPaymentIntent API

---
+++ functions/api.js
@@ -0,0 +1,26 @@
+export function createCharge (req, res) {
+    const total = req.query.total;
+    console.log(`payment request received for total ${total}`);
+    if (!total) {
+        res.status(400).send({
+            status: false,
+            message: 'Invalid total amount passed'
+        })
+    }
+    try {
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount: total,  //total value must be in subunits of the currency
+            currency: 'cad'
+        });
+        console.log(paymentIntent);
+        res.status(201).send({
+            status: true,
+            clientSecret: paymentIntent.client_secret
+        });
+    } catch (err) {
+        console.error(err);
+        res.send({
+            message: err
+        })
+    }
+}
